537d16167631e3fe60de2202e22f8b74f6c87ca3
deadmedusa <zhidkov@hotmail.com>
2025-08-24 17:44:49 +0100

    Initial commit - React app ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+﻿import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>🚀 Success!</h1>
+        <p>My React App is deployed on cPanel!</p>
+        <p>This was deployed via CI/CD from GitHub!</p>
       </header>
     </div>
   );
